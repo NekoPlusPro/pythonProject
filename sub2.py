3b99d38920c29299c0484543777bb753f91c8173
neko-tal <jiax_chen@outlook.com>
2021-07-28 20:16:20 +0800

    筛选修正

--- sub2.py
+++ sub2.py
@@ -1,12 +1,13 @@
 import csv
 import re
 
-rname = '220102.csv'
-wname = 'result3.csv'
+cityname = "bj"
+rname = cityname + 'bu.csv'
+wname = 'result_' + cityname + '.csv'
 sieve = ["山庄", "庄园", "学院", "小区",
          "家园", "花园", "国际", "别墅",
          "中心", "广场", "公馆", "公园",
-         "大厦", "小镇", "公寓", "社区", "嘉园"]
+         "大厦", "小镇", "公寓", "社区", "嘉园","属区"]
 
 
 def lcsubstr(s1, s2):
@@ -25,15 +26,15 @@ def lcsubstr(s1, s2):
 
 def regu(t):
     a = re.sub("\\(.*?\\)", "", t)
-    a = re.sub("[·.|-]","",a)
+    a = re.sub("[·.|-]", "", a)
     for i in sieve:
-        a = re.sub(i, "", a)
+        a = a[:-2]+re.sub(i, "", a[-2:])
     return a
 
 
 def uncn(f):
     pat = re.compile(r'[\u4e00-\u9fa5]')
-    b = re.sub("[·.|-]"," ",f)
+    b = re.sub("[·'\".|-]", " ", f)
     return '|'.join(re.sub(pat, " ", b).split())
 
 
@@ -42,13 +43,15 @@ with open(rname) as r:
         reader = csv.reader(r)
         writer = csv.writer(w)
         header_row = next(reader)
-        header_row.extend(["re_res_name",'re_len', "appr", "subname"])
+        header_row.extend(["re_res_name", 're_len', "appr", "subname", "appr_score", "to_score"])
         writer.writerow(header_row)
         print(header_row)
         for row in reader:
-            t=regu(row[5])
+            t = regu(row[5])
             row.append(t)
             row.append(len(t))
-            row.append(lcsubstr(re.sub("[·.|-]","",row[4]), row[8])[1])
-            row.append(uncn(row[4]))
+            row.append(lcsubstr(re.sub("[·'\".|-]", "", row[4]), row[8])[1])
+            row.append(uncn(re.sub("[·'\".-]", "|", row[4])))
+            row.append(row[10] * 7.0 / row[9] if row[9] > 1 and row[10] > 1 else 0)
+            row.append(row[12] + float(row[7]))
             writer.writerow(row)
